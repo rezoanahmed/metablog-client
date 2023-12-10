66a7020fd6f26b5f340212678daa252a1b056a77
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-12-10 17:41:43 +0600

    minor style changed in hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
                 <img src="https://i.ibb.co/FzdFJ5s/4394e91108e011b0d07581596959713b.jpg" alt="" className="h-[500px] w-full object-cover brightness-50 rounded-md" />
             </div>
             <div className="absolute p-8 rounded-md bg-white max-w-md -mt-20 ml-20 shadow-md">
-                <p className="bg-blue-700 text-white badge">Technology</p>
+                <button className="btn btn-sm bg-blue-600 hover:bg-blue-600 text-white">Technology</button>
                 <p className="text-2xl font-bold">The Inpact of Technology on the Workplace: How Technology is Changing</p>
             </div>
         </>
