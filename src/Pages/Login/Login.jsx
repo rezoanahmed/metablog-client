566f947ab5d07afb9ab93d12d9a82ee9123be2b8
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-12-10 21:16:15 +0600

    implemented google login system, registration and login into login and register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,15 +1,70 @@
 import { NavLink } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
-
+import { useContext } from "react";
+import { AuthContext } from "../../Context/ContextProvider";
+import Swal from "sweetalert2";
 
 const Login = () => {
+    const { login, googleLogin } = useContext(AuthContext);
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        // console.log(email, password);
+        login(email, password)
+            .then(userCredentials => {
+                if (userCredentials) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Welcome!",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(err=>{
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${err.code}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(userCredentials => {
+                if (userCredentials) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Welcome!",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(err=>{
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${err.code}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-base-300">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card bg-white p-8 shadow-xl">
-                        <form className="">
+                        <form onSubmit={handleLogin}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -32,7 +87,7 @@ const Login = () => {
                         <div>
                             <div className="divider">Or</div>
                             <div className="flex justify-center items-center gap-5">
-                                <button className="btn rounded-full text-lg"><FcGoogle></FcGoogle></button>
+                                <button onClick={handleGoogleLogin} className="btn rounded-full text-lg"><FcGoogle></FcGoogle></button>
                                 <button className="btn rounded-full text-blue-600 text-lg"><FaFacebook></FaFacebook></button>
                             </div>
                         </div>
