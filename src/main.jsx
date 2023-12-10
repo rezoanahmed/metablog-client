c5d7558f6b6bd1f48a5c63753203fa06d9969acc
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-12-10 16:18:26 +0600

    fetched the posts dynamically from mongodb using axios and tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './Routes/Routes.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes}></RouterProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
